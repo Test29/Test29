0933164d19ee39bd65e02f50527d031925293123
Antoine <antoine.bienvenusup@gmail.com>
2012-09-18 10:26:14 +0200

    structure

--- protected/views/school/_view.php
+++
@@ -1,6 +0,0 @@
-<div class="view">
-	<h3><?php echo CHtml::link(CHtml::encode($data->name), array('view', 'id'=>$data->id)); ?></h3>	
-	<div>	
-		<img src="<?php echo Yii::app()->baseUrl ?><?php echo $data->picture->url; ?>" alt="picture_school"/>	
-	</div>	
-</div>
