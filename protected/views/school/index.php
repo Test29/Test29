e1fc70e14be7c7bca59eec25fa0b98a179753240
Club2014 <antoine-bienvenu@sfr.fr>
2012-09-25 11:13:57 +0200

    Bug quand on est pas connecté

--- protected/views/school/index.php
+++ protected/views/school/index.php
@@ -16,7 +16,7 @@ $this->breadcrumbs=array(
 </div>
 <?php endif; ?>
 <h1>Ecole</h1>
-<?php if ($_SESSION['right'] == 'admin' || $_SESSION['right'] == 'responsable'){ ?>
+<?php if (isset($_SESSION['id']) && ($_SESSION['right'] == 'admin' || $_SESSION['right'] == 'responsable')){ ?>
 <?php $this->widget('bootstrap.widgets.TbButton', array(
     'label'=>'Nouvelle école',
     'type'=>'success',
@@ -25,34 +25,46 @@ $this->breadcrumbs=array(
 )); ?>
 <br /><br />
 <?php } ?>
-
-<?php foreach ($schools as $key => $school) { ?>
-    <ul class="thumbnails">
-    <li class="span4">
-    <div class="thumbnail">
-    <img src="<?php echo $school['url']; ?>" alt="">
-    <h3><?php echo CHtml::link($school['name'], array('view', 'id'=>$school['id'])); ?></h3>
-    <p><?php echo $school['description']; ?></p>
-    <?php if ($_SESSION['right'] == 'admin' || $_SESSION['right'] == 'responsable'){ ?>
-    <?php $this->widget('bootstrap.widgets.TbButton', array(
-    'label'=>'Modifier',
-    'type'=>'info',
-    'size'=>'medium',
-    'url'=>array('/school/update/'.$school['id']),
-    )); ?>
-    <?php $this->widget('bootstrap.widgets.TbButton', array(
-    'label'=>'Supprimer',
-    'type'=>'danger',
-    'size'=>'medium',
-    'url'=>array('/school/delete/'.$school['id']),
-    'htmlOptions'=>array('confirm'=> 'Etes vous sûr de vouloir supprimer cette école ?'),
-    )); ?>
+<?php if (isset($_SESSION['id'])) { ?>
+    <?php foreach ($schools as $key => $school) { ?>
+	<ul class="thumbnails">
+	<li class="span4">
+	<div class="thumbnail">
+	<img src="<?php echo $school['url']; ?>" alt="">
+	<h3><?php echo CHtml::link($school['name'], array('view', 'id'=>$school['id'])); ?></h3>
+	<p><?php echo $school['description']; ?></p>
+	<?php if ($_SESSION['right'] == 'admin' || $_SESSION['right'] == 'responsable'){ ?>
+	<?php $this->widget('bootstrap.widgets.TbButton', array(
+	'label'=>'Modifier',
+	'type'=>'info',
+	'size'=>'medium',
+	'url'=>array('/school/update/'.$school['id']),
+	)); ?>
+	<?php $this->widget('bootstrap.widgets.TbButton', array(
+	'label'=>'Supprimer',
+	'type'=>'danger',
+	'size'=>'medium',
+	'url'=>array('/school/delete/'.$school['id']),
+	'htmlOptions'=>array('confirm'=> 'Etes vous sûr de vouloir supprimer cette école ?'),
+	)); ?>
+	<?php } ?>
+	</div>
+	</li>
+	</ul>
     <?php } ?>
-    </div>
-    </li>
-    </ul>
+<?php } else { ?>
+    <?php foreach ($schools as $key => $school) { ?>
+	<ul class="thumbnails">
+	<li class="span4">
+	<div class="thumbnail">
+	<img src="<?php echo $school['url']; ?>" alt="">
+	<h3><?php echo $school['name']; ?></h3>
+	<p><?php echo $school['description']; ?></p>
+	</div>
+	</li>
+	</ul>
+    <?php } ?>
 <?php } ?>
-
 
 
 
